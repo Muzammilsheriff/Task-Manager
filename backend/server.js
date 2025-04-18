fc8238ea9ed272ece83bce09f07ab620bf4218cf
MuzammilSpie <amuzammil606@gmail.com>
2025-04-18 15:29:50 +0530

    Create server.js

---
+++ backend/server.js
@@ -0,0 +1,37 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const connectDB = require("./config/db");
+
+let adminRoutes = require("./routes/adminRoutes");
+let userRoutes = require("./routes/userRoutes");
+let taskRoutes = require("./routes/taskRoutes");
+const app = express();
+
+// Connect to MongoDB
+connectDB();
+
+// Middleware to Handle Cors
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL || "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
+// Middleware
+app.use(express.json());
+
+// Routes
+app.use("/api/auth", adminRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/task", taskRoutes);
+// app.use("/api/report", require("./routes/report"))
+
+// Start the server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
