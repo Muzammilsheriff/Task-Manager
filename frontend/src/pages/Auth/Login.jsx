88b35fbf73271e35249c87a8a5d83270ccc32e56
MuzammilSpie <amuzammil606@gmail.com>
2025-05-12 14:54:26 +0530

    Authorization Pages

--- frontend/src/pages/Auth/Login.jsx
+++ frontend/src/pages/Auth/Login.jsx
@@ -1,9 +1,111 @@
-import React from 'react'
-
+import AuthLayout from '../../components/Layouts/AuthLayout'
+import { React, useState, useContext } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import Input from '../../components/Inputs/Input'
+import { validateEmail } from '../../utils/helper';
+import axiosInstance from '../../utils/axiosInstance';
+import { API_PATHS } from '../../utils/apiPaths';
+import  {UserContext}  from '../../context/userContext';
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [error, setError] = useState('')
+  const{ updateUser } = useContext(UserContext)
+  const navigate = useNavigate();
+
+  // Handle Login Form Submission
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    if (!validateEmail(email)) {
+      setError('Please enter a valid email address')
+      return
+    }
+
+    if (!password) {
+      setError('Please enter the password')
+      return
+    }
+    if (password.length < 8) {
+      setError('Password should be at least 8 characters long')
+      return
+    }
+
+    setError('')
+
+    // Login Api Call
+    try {
+      const res = await axiosInstance.post(API_PATHS.AUTH.LOGIN, {
+        email,
+        password
+      });
+
+      const { token, role } = res.data
+      console.log(res.data);
+      
+
+      if (token) {
+        localStorage.setItem('token', token)
+        updateUser(res.data) // Assuming you have a function to update user context
+
+        // Redirect based on role
+        if (role === 'admin') {
+          navigate('/admin/dashboard')
+        }
+        else if (role === 'member') {
+          navigate('/user/dashboard')
+        }
+
+      }
+    }
+
+    catch (error) {
+      if (error.response && error.response.data.message ) {
+        setError(error.response.data.message)
+      } else {
+        setError('Something went wrong. Please try again later.')
+      }
+    }
+
+  }
+
+
+return <AuthLayout>
+  <div className='lg:w-[70%] h-3/4 md:h-full flex flex-col justify-center'>
+    <h3 className='text-xl font-semibold text-black'>Welcome Back</h3>
+    <p className='text-sm text-slate-700 mt-[5px] mb-6'>Please enter your details to login </p>
+
+    <form onSubmit={handleLogin}>
+      <Input
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        type="text"
+        placeholder="john@example.com"
+        label="Email Address"
+      />
+      <Input
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        label="Password"
+        placeholder="Min 8 characters"
+        type="password"
+      />
+
+      {error && <p className='text-red-600 text-sm mt-2'>{error}</p>}
+
+
+      <button type='submit' className='btn-primary'>LOGIN</button>
+
+      <p className='text-[13px] text-slate-800 mt-4'>
+        Don't Have an Account?{"  "}
+        <Link className='underline font-medium text-primary ' to='/signup'>
+          SignUp
+        </Link>
+      </p>
+    </form>
+
+  </div>
+</AuthLayout>
 }
 
 export default Login
--- frontend/src/pages/Auth/SignUp.jsx
+++ frontend/src/pages/Auth/SignUp.jsx
@@ -1,9 +1,159 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
+import AuthLayout from '../../components/Layouts/AuthLayout'
+import { validateEmail } from '../../utils/helper'
+import ProfilePhotoSelector from '../../components/Inputs/ProfilePhotoSelector'
+import Input from '../../components/Inputs/Input'
+import { Link, useNavigate } from 'react-router-dom'
+import axiosInstance from '../../utils/axiosInstance'
+import { API_PATHS } from '../../utils/apiPaths'
+import { UserContext } from '../../context/userContext'
+import uploadImage from '../../utils/uploadImage'
 
 const SignUp = () => {
-  return (
-    <div>SignUp</div>
-  )
-}
 
-export default SignUp
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [fullName, setFullName] = useState('')
+  const [profilePic, setProfilePic] = useState('')
+  const [adminInviteCode, setAdminInviteCode] = useState('')
+  const [error, setError] = useState(null)
+  const{ updateUser } = useContext(UserContext)
+  const navigate = useNavigate();
+
+  const handleSignup = async (e) => {
+    e.preventDefault();
+
+    let profileImageURL = ''
+
+    if (!fullName) {
+      setError('Please enter a full name')
+      return
+    }
+
+    if (!validateEmail(email)) {
+      setError('Please enter a valid email address')
+      return
+    }
+
+    if (!password) {
+      setError('Please enter the password')
+      return
+    }
+    if (password.length < 8) {
+      setError('Password should be at least 8 characters long')
+      return
+    }
+
+    setError('')
+
+    // Signup Api Call
+    try {
+
+      // Upload profile image if provided
+      if (profilePic) {
+        const imgUploadRes = await uploadImage(profilePic)
+        profileImageURL = imgUploadRes.imageURL || ''
+      }
+      console.log(profileImageURL);
+      
+
+      const response = await axiosInstance.post(API_PATHS.AUTH.REGISTER, {
+        name: fullName,
+        email,
+        password,
+        profilePic: profileImageURL,
+        adminInviteCode
+      });
+
+      const { token, role } = response.data
+
+      if(token) {
+        localStorage.setItem('token', token)
+        updateUser(response.data) 
+
+        // Redirect based on role
+        if (role === 'admin') {
+          navigate('/admin/dashboard')
+        }
+        else{
+          navigate('/user/dashboard')
+        }
+      }
+    }
+
+    catch (error) {
+        if (error.response && error.response.data.message) {
+          setError(error.response.data.message)
+        } else {
+          setError('Something went wrong. Please try again later.')
+        }
+      }
+
+    };
+
+
+
+    return (
+      <AuthLayout>
+        <div className='lg:w-[100%] h-auto md:h-full mt-10 md:mt-10 flex flex-col justify-center'>
+          <h3 className='text-xl font-semibold text-black'>Create an Account</h3>
+          <p className='text-xs text-slate-700 mt-[5px] mb-6'>
+            Join us today by enterning your details below
+          </p>
+
+
+          <form onSubmit={handleSignup}>
+            <ProfilePhotoSelector image={profilePic} setImage={setProfilePic} />
+
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-4 ">
+              <Input
+                value={fullName}
+                onChange={({ target }) => setFullName(target.value)}
+                type="text"
+                placeholder="John Doe"
+                label="Full Name"
+              />
+              <Input
+                value={email}
+                onChange={({ target }) => setEmail(target.value)}
+                type="text"
+                placeholder="john@example.com"
+                label="Email Address"
+              />
+              <Input
+                value={password}
+                onChange={({ target }) => setPassword(target.value)}
+                label="Password"
+                placeholder="Min 8 characters"
+                type="password"
+              />
+              <Input
+                value={adminInviteCode}
+                onChange={({ target }) => setAdminInviteCode(target.value)}
+                label="Admin Invite Code"
+                placeholder="Enter code if you have one"
+                type="text"
+              />
+            </div>
+            {error && <p className='text-red-600 text-sm mt-2'>{error}</p>}
+
+
+            <button type='submit' className='btn-primary'>SIGN UP</button>
+
+            <p className='text-[13px] text-slate-800 mt-4'>
+              Already have an Account?{"  "}
+              <Link className='underline font-medium text-primary ' to='/login'>
+                Login
+              </Link>
+            </p>
+          </form>
+
+
+        </div>
+
+
+      </AuthLayout>
+    )
+  }
+
+  export default SignUp
