1fdd419c1f5ac84d540f30d82b78d7e9e92ebafc
MuzammilSpie <amuzammil606@gmail.com>
2025-06-09 14:10:54 +0530

    Layouts

---
+++ frontend/src/components/Layouts/AuthLayout.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+// import UI_IMG from
+import back from '../../assets/images/background.jpg'
+
+const AuthLayout = ({ children }) => {
+    return <div className="flex">
+        <div className='w-screen h-screen md:w-[60vw] px-12 pt-8 pb-12'>
+            <h2 className='text-lg font-medium text-black '>Task Manager</h2>
+            {children}
+        </div>
+
+
+        <div className="hidden md:flex w-[40vw] h-screen items-center justify-center bg-blue-50 bg-cover bg-no-repeat bg-center" style={{ backgroundImage: `url(${back})` }}>
+            {/* <img src='' className='w-64 lg:w-[90%]'></img> */}
+        </div>
+    </div>
+
+}
+
+export default AuthLayout
