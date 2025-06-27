f9e1bfa9bd61c832e5d23dabbf5f05b5c9896398
MuzammilSpie <amuzammil606@gmail.com>
2025-06-28 01:56:31 +0530

    Update taskModel.js

--- backend/models/taskModel.js
+++ backend/models/taskModel.js
@@ -26,7 +26,7 @@ const taskSchema = new mongoose.Schema({
     },
     status: {
         type: String,
-        enum: ["Pending", "In-Progress", "Completed"],
+        enum: ["Pending", "In Progress", "Completed"],
         default: "Pending"
     },
     dueDate: {
